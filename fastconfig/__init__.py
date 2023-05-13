9f653fd8a28b2378fe2548ffe4f24a68d883dee2
kyoto7250 <50972773+kyoto7250@users.noreply.github.com>
2023-05-13 22:01:28 +0900

    add methods for checking the project root and getting file

---
+++ fastconfig/__init__.py
@@ -0,0 +1,104 @@
+import os
+from pathlib import Path
+from typing import List, Optional, Union
+
+__version__ = "0.1.0"
+PROJECT_ROOTS: List[str] = [".hg", ".git"]
+DEPTH: int = 10
+
+
+def is_project_root(path: Union[str, Path]) -> bool:
+    """
+    Check the given path is the project root directory or not.
+    This method determines the project root by whether the version control tool directory exists.
+
+    Args:
+        path (Union[str, Path]): The path to check whether it is the project root or not
+
+    Returns:
+        bool: The result of the project root or not
+    """
+    if isinstance(path, str):
+        path = Path(path)
+
+    if path.is_file():
+        path = path.parent
+
+    for candidate in PROJECT_ROOTS:
+        if path.joinpath(candidate).is_dir():
+            return True
+    return False
+
+
+def find_project_root(path: Optional[Union[str, Path]] = None) -> Optional[Path]:
+    """
+    Return if the project root is found, or None if not
+
+    Args:
+        path (Optional[Union[str, Path]]): A path string or Path object to start searching, If nothing is passed, start in the current directory
+
+    Returns:
+        Optional[Path]: the project root path, or None if the project root is not found
+    """
+    if path is None:
+        path = os.getcwd()
+    cnt: int = 0
+
+    candidate: Path = Path(path) if isinstance(path, str) else path
+    if is_project_root(candidate):
+        return candidate
+
+    while cnt < DEPTH:
+        candidate = candidate.parent
+        if is_project_root(candidate):
+            return candidate
+
+        if candidate.parent == candidate:
+            return None
+
+        cnt += 1
+    return None
+
+
+def search(
+    target: Union[str, Path],
+    path: Optional[Union[str, Path]] = None,
+    end_up_the_project_root: bool = True,
+) -> Optional[Path]:
+    """
+    Recursively searches for files with the name of the target and returns the result
+
+    Args:
+        target (Union[str, Path]): Search target filename, and directory names are ignored
+        path (Optional[Union[str, Path]]): A path string or Path object to start searching, If nothing is passed, start in the current directory
+        end_up_the_project_root (bool): Whether or not to search the directory where the version control tool exists
+
+    Returns:
+        Optional[Path]: a path of the target file, or None if the target file is not found
+    """
+    if isinstance(target, str):
+        target = Path(target)
+
+    target_name: str = target.name
+    if path is None:
+        path = os.getcwd()
+
+    cnt: int = 0
+    candidate: Path = Path(os.path.join(path, target_name))
+    if candidate.exists():
+        return candidate
+    if is_project_root(candidate):
+        return None
+
+    while cnt < DEPTH:
+        candidate = candidate.parent.parent.joinpath(target_name)
+        if candidate.exists():
+            return candidate
+
+        if candidate.parent.parent.joinpath(target_name) == candidate or (
+            end_up_the_project_root and is_project_root(candidate)
+        ):
+            return None
+
+        cnt += 1
+    return None
---
+++ tests/test_init.py
@@ -0,0 +1,80 @@
+import unittest
+from pathlib import Path
+
+from fastconfig import find_project_root, is_project_root, search
+
+
+class TestSearch(unittest.TestCase):
+    def test_search(self) -> None:
+        # file search
+        self.assertIsNotNone(search(target="README.md"))
+        self.assertIsNotNone(search(target="README.md", path="tests/"))
+        self.assertIsNotNone(
+            search(target="README.md", path="tests/", end_up_the_project_root=False)
+        )
+        self.assertIsNone(
+            search(
+                target="README.md",
+                path="./a/b/c/d/e/f/g/h/i/j/k/l/m/n",
+                end_up_the_project_root=False,
+            )
+        )
+
+        # not found file search
+        self.assertIsNone(search(target="README.txt"))
+        self.assertIsNone(search(target="README.txt", path="tests/"))
+        self.assertIsNone(
+            search(target="README.txt", path="tests/", end_up_the_project_root=False)
+        )
+
+        # directory search
+        self.assertIsNotNone(search(target=".git/"))
+        self.assertIsNotNone(search(target=".git/", path="tests/"))
+        self.assertIsNotNone(
+            search(target=".git/", path="tests/", end_up_the_project_root=False)
+        )
+        self.assertIsNone(
+            search(
+                target=".git/",
+                path="./a/b/c/d/e/f/g/h/i/j/k/l/m/n",
+                end_up_the_project_root=False,
+            )
+        )
+
+        # not found directory search
+        self.assertIsNone(search(target=".hg/"))
+        self.assertIsNone(search(target=".hg/", path="tests/"))
+        self.assertIsNone(
+            search(target=".hg/", path="tests/", end_up_the_project_root=False)
+        )
+
+    def test_is_project_root(self) -> None:
+        # str
+        self.assertTrue(is_project_root("./"))
+        self.assertTrue(is_project_root("./README.md"))
+        self.assertFalse(is_project_root("tests/"))
+        self.assertFalse(is_project_root("tests/README.txt"))
+
+        # Path
+        self.assertTrue(is_project_root(Path("./")))
+        self.assertFalse(is_project_root(Path("./hoge.txt")))
+        self.assertFalse(is_project_root(Path("tests/")))
+        self.assertFalse(is_project_root(Path("tests/hoge.txt")))
+
+    def test_find_project_root(self) -> None:
+        # str
+        self.assertIsNotNone(find_project_root("./"))
+        self.assertIsNotNone(find_project_root("./tests"))
+        self.assertIsNone(find_project_root("./a/b/c/d/e/f/g/h/i/j/k/j/l/m/n"))
+
+        # Path
+        self.assertIsNotNone(find_project_root(Path("./")))
+        self.assertIsNotNone(find_project_root(Path("./tests")))
+        self.assertIsNone(find_project_root(Path("./a/b/c/d/e/f/g/h/i/j/k/j/l/m/n")))
+
+        # None
+        self.assertIsNotNone(find_project_root())
+
+
+if __name__ == "__main__":
+    unittest.main()
